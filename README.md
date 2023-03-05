49eab1c69a0e469616ae8491631e2806cbd31d6a
Shamim-hossain12 <mdshamimhossain62273295@gmail.com>
2023-03-05 14:14:07 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# JS-all-country-area-code
---
+++ main.js
@@ -0,0 +1,100 @@
+let out = document.querySelector('.result');
+var x = 'country';
+
+function type_country(x) {
+    if (x == 'bangladesh' || x == 'bd') {
+        out.innerHTML = 880;
+    } else if (x == 'bahrain' || x == 'bh') {
+        out.innerHTML = 973;
+    } else if (x == 'bhutan' || x == 'bt') {
+        out.innerHTML = 975;
+    } else if (x == 'brunei' || x == 'bn') {
+        out.innerHTML = 673;
+    } else if (x == 'cambodia' || x == 'kh') {
+        out.innerHTML = 855;
+    } else if (x == 'china' || x == 'cn') {
+        out.innerHTML = 86;
+    } else if (x == 'east timor' || x == 'tl') {
+        out.innerHTML = 670;
+    } else if (x == 'hong kong' || x == 'hk') {
+        out.innerHTML = 852;
+    } else if (x == 'india' || x == 'in') {
+        out.innerHTML = 91;
+    } else if (x == 'indonesia' || x == 'id') {
+        out.innerHTML = 62;
+    } else if (x == 'iran' || x == 'ir') {
+        out.innerHTML = 98;
+    } else if (x == 'iraq' || x == 'iq') {
+        out.innerHTML = 964;
+    } else if (x == 'israel' || x == 'il') {
+        out.innerHTML = 972;
+    } else if (x == 'japan' || x == 'jp') {
+        out.innerHTML = 81;
+    } else if (x == 'jordan' || x == 'jo') {
+        out.innerHTML = 962;
+    } else if (x == 'kazakhstan' || x == 'kz') {
+        out.innerHTML = 7;
+    } else if (x == 'kuwait' || x == 'kw') {
+        out.innerHTML = 965;
+    } else if (x == 'kyrgyzstan' || x == 'kg') {
+        out.innerHTML = 996;
+    } else if (x == 'laos' || x == 'la') {
+        out.innerHTML = 856;
+    } else if (x == 'lebanon' || x == 'lb') {
+        out.innerHTML = 961;
+    } else if (x == 'macau' || x == 'mo') {
+        out.innerHTML = 853;
+    } else if (x == 'malaysia' || x == 'my') {
+        out.innerHTML = 60;
+    } else if (x == 'maldives' || x == 'mv') {
+        out.innerHTML = 960;
+    } else if (x == 'mongolia' || x == 'mn') {
+        out.innerHTML = 976;
+    } else if (x == 'myanmar' || x == 'mm') {
+        out.innerHTML = 95;
+    } else if (x == 'nepal' || x == 'np') {
+        out.innerHTML = 977;
+    } else if (x == 'north korea' || x == 'kp') {
+        out.innerHTML = 850;
+    } else if (x == 'oman' || x == 'om') {
+        out.innerHTML = 968;
+    } else if (x == 'pakistan' || x == 'pk') {
+        out.innerHTML = 92;
+    } else if (x == 'palestine' || x == 'ps') {
+        out.innerHTML = 970;
+    } else if (x == 'philippines' || x == 'ph') {
+        out.innerHTML = 63;
+    } else if (x == 'qatar' || x == 'qa') {
+        out.innerHTML = 974;
+    } else if (x == 'saudi arabia' || x == 'sa') {
+        out.innerHTML = 966;
+    } else if (x == 'singapore' || x == 'sg') {
+        out.innerHTML = 65;
+    } else if (x == 'south korea' || x == 'kr') {
+        out.innerHTML = 82;
+    } else if (x == 'sri lanka' || x == 'lk') {
+        out.innerHTML = 94;
+    } else if (x == 'syria' || x == 'sy') {
+        out.innerHTML = 963;
+    } else if (x == 'taiwan' || x == 'tw') {
+        out.innerHTML = 886;
+    } else if (x == 'tajikistan' || x == 'tj') {
+        out.innerHTML = 992;
+    } else if (x == 'thailand' || x == 'th') {
+        out.innerHTML = 66;
+    } else if (x == 'turkey' || x == 'tr') {
+        out.innerHTML = 90;
+    } else if (x == 'turkmenistan' || x == 'tm') {
+        out.innerHTML = 993;
+    } else if (x == 'united arab emirates' || x == 'ae') {
+        out.innerHTML = 971;
+    } else if (x == 'uzbekistan' || x == "uz") {
+        out.innerHTML = 998;
+    } else if (x == 'vietnam' || x == 'vn') {
+        out.innerHTML = 84;
+    } else if (x == 'yemen' || x == 'ye') {
+        out.innerHTML = 84;
+    } else {
+        out.innerHTML = 'invalid';
+    }
+}
